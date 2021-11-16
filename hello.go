1d787d5de6dde471fd3baa6b27f951c9bb835567
Sasha Sokolov <sashasokolov777@gmail.com>
2021-11-16 21:23:14 +0200

    add a case for Spanish

--- hello.go
+++ hello.go
@@ -5,10 +5,17 @@ import "fmt"
 const englishHelloPrefix = "Hello, "
 
 
-func Hello(s string) string {
-	return englishHelloPrefix + s
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	if language == "Spanish" {
+		return "Hola, " + name
+	}
+	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println("Hello world!")
+	fmt.Println(Hello("Chris", ""))
 }
