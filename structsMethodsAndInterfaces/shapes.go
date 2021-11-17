9583aa0d5e1e1bfaa7a36770b257db1c5263ff44
Sasha Sokolov <sashasokolov777@gmail.com>
2021-11-18 01:48:56 +0200

    style edits

--- structsMethodsAndInterfaces/shapes.go
+++ structsMethodsAndInterfaces/shapes.go
@@ -2,18 +2,17 @@ package main
 
 import "math"
 
-
 type Shape interface {
 	Area() float64
 }
 
 type Rectangle struct {
-	Width float64
+	Width  float64
 	Height float64
 }
 
 type Triangle struct {
-	Base float64
+	Base   float64
 	Height float64
 }
 
@@ -30,7 +29,7 @@ func (c Circle) Area() float64 {
 }
 
 func (t Triangle) Area() float64 {
-	return (t.Base * t.Height) *0.5
+	return (t.Base * t.Height) * 0.5
 }
 
 func Perimeter(r Rectangle) float64 {
--- structsMethodsAndInterfaces/shapes_test.go
+++ structsMethodsAndInterfaces/shapes_test.go
@@ -2,7 +2,7 @@ package main
 
 import "testing"
 
-func TestPerimeter(t *testing.T){
+func TestPerimeter(t *testing.T) {
 	rectangle := Rectangle{10.0, 10.0}
 	got := Perimeter(rectangle)
 	want := 40.0
@@ -12,12 +12,11 @@ func TestPerimeter(t *testing.T){
 	}
 }
 
-
-func TestArea(t *testing.T){
-	areaTests := []struct{
-		name string
+func TestArea(t *testing.T) {
+	areaTests := []struct {
+		name  string
 		shape Shape
-		want float64
+		want  float64
 	}{
 		{name: "Rectangle", shape: Rectangle{12.0, 3.0}, want: 36.0},
 		{name: "Circle", shape: Circle{10}, want: 314.1592653589793},
